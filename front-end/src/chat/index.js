0c3230ea7316df3daee232acd0531852702fdb4e
mrnazu <samuelamsalu1998@gmail.com>
2023-12-21 11:09:43 +0300

    Sidebard/chat histroy correction

--- front-end/src/chat/index.js
+++ front-end/src/chat/index.js
@@ -4,33 +4,63 @@ const Chat = () => {
   return (
     <div className="h-screen w-screen bg-[black]">
       <div className="w-[20%] h-screen bg-[#1b1a1a] text-white p-4">
-        <div>
+        <div className="h-[5%]">
           <button className="w-full h-[50px] border rounded">New Chat</button>
         </div>
-        <div className="h-[75%]">
-          <div className="py-3 rounded text-center mt-5 font-light flex items-center hover:bg-[black] px-8 cursor-pointer">
-            <span className="mr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon icon-tabler icon-tabler-messages"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
-              </svg>
-            </span>
-            My Chat history
-          </div>
+        <div className="h-[75%] mt-8 overflow-scroll hide-scroll-bar">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item, index) => {
+            return (
+              <div className="py-3 rounded text-center mt-5 font-light flex items-center hover:bg-[black] px-8 cursor-pointer">
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-messages"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
+                    <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
+                  </svg>
+                </span>
+                My Chat history
+              </div>
+            );
+          })}
         </div>
-        <div></div>
+        <div className="h-[20%] overflow-scroll hide-scroll-bar border-t">
+          {[1, 2].map((item, index) => {
+            return (
+              <div className="py-3 rounded text-center mt-5 font-light flex items-center px-8 cursor-pointer">
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-messages"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
+                    <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
+                  </svg>
+                </span>
+                My Chat history
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="w[80%]"></div>
     </div>
